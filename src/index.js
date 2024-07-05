581a739ac0fb747c1f46bd6f9041e59b6964be43
hyunjiq <edalnim12@gmail.com>
2024-07-05 11:50:27 +0900

    부모태그에서 자식태그로 매개인자 변수로 저장

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App'; //상대경로는 모두 src 집에서 찾기
+import App from './Child'; //상대경로는 모두 src 집에서 찾기
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <App msg="나 전달 되었나?" num="20"/>
 );
 
 
