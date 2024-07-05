f251b21b0d03f166401880644221673439cd390d
hyunjiq <edalnim12@gmail.com>
2024-07-05 10:55:02 +0900

    나의 버셀에 올리기

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import logo from "./logo.svg"
+
+const App = () => {
+  return (
+    <header id="hd" className="border-bottom">
+      <div className="container d-flex justify-content-between align-items-center">
+        <h1 className="col-4 col-md-2">
+          <a href="" className="d-block">
+            <img src={logo} className="w-100" alt="" />
+          </a>
+          </h1>
+        <ul id="gnb" className="d-flex">
+          <li><a href="">대메뉴</a></li>
+          <li><a href="">대메뉴</a></li>
+          <li><a href="">대메뉴</a></li>
+          <li><a href="">대메뉴</a></li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App'; //상대경로는 모두 src 집에서 찾기
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
+
